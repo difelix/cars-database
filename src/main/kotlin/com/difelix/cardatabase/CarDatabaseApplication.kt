259a51df6116477a5927b099a3fdf5c1910263f6
Diego Félix da Silva <57463258+difelix@users.noreply.github.com>
2021-02-09 21:32:42 -0300

    commit da estrutura inicial do projeto (#1)

---
+++ src/main/kotlin/com/difelix/cardatabase/CarDatabaseApplication.kt
@@ -0,0 +1,11 @@
+package com.difelix.cardatabase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarDatabaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarDatabaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/difelix/cardatabase/CarDatabaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.difelix.cardatabase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarDatabaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
